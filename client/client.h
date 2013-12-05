3a95353a697b4f35bf70850a0c3ca575b275a4eb
rgupta17 <rgupta17@vt.edu>
2013-12-05 08:44:34 -0500

    Update client.h

--- client/client.h
+++ client/client.h
@@ -1,130 +1,149 @@
+#ifndef CLIENT_H
+#define CLIENT_H
 
- #ifndef CLIENT_H
- #define CLIENT_H
+#include <QTcpSocket>
+#include <QObject>
+#include <QString>
+#include <QMap>
 
- #include <QDialog>
- #include <QTcpSocket>
+class QTcpSocket;
+class QNetworkSession;
 
- class QDialogButtonBox;
- class QLabel;
- class QLineEdit;
- class QPushButton;
- class QTcpSocket;
- class QNetworkSession;
+class Client : public QObject
+{
+    Q_OBJECT
 
- /**
-  * @brief Client object.
-  *
-  * This client object is meant to provide a GUI in order to communicate with a Server.
-  * This GUI is lightweight in comparison, providing only socket communication and
-  * through an interface.  The server that this connects to performs all major operations.
-  *
-  */
- class Client : public QDialog
- {
-     Q_OBJECT
+public:
+    Client(QObject *parent = 0);
 
- public:
-/**
- * @brief The constructor for a client object.
- *
- * This constructor builds the client object by setting up the entire GUI here and starting
- * a Network Manager.
- *
- * @param parent Initializes the server with a qobject parent if desired
- * or can be instantiated without if desired.
- */
-     Client(QWidget *parent = 0);
+public slots:
+    /**
+     * @brief sendMsg sends a command
+     *
+     * This sends a message to the server from the GUI
+     *
+     * @param message The message from the GUI
+     */
+    void sendMsg(QString message);
 
- private slots:
-     /**
-      * @brief Controls sending a command to the server.
-      *
-      * First, this disables the push button and then sets up the communication with the server.
-      * The communication is only set up if it currently isn't open. The communication is closed
-      * every time this client quits or the server quits.  Last, this writes data to the socket.
-      *
-      */
-     void sendCmd();
-     /**
-      * @brief Reads the results sent back by the server through the socket.
-      *
-      * This is set up through the readyRead() signal to run and continutously read back data.
-      * The amount of data read back is identified by the first 16 bits read through the socket
-      * on each new transfer
-      *
-      */
-     void readResults();
-     /**
-      * @brief Displays any error in reading from the socket.
-      *
-      * Displays relevant error information in a popup message box.
-      *
-      * @param socketError the error seen on the socket.
-      */
-     void displayError(QAbstractSocket::SocketError socketError);
-     /**
-      * @brief Determines whether or not to enable the sendCmdButton.
-      *
-      * This looks at connection information as well as text that's been typed in to make
-      * sure relevant information has been provided and that the action can actually do something
-      * if clicked.
-      *
-      */
-     void enableSendCmdButton();
-     /**
-      * @brief Sets up settings and enables sending commands.
-      *
-      * This only runs if the network manager was able to open up the session.  Enables
-      * the sendCmdButton if the session was opened (and if the other requirements are met).
-      *
-      */
-     void sessionOpened();
-     /**
-      * @brief Disconnects this socket from the server.
-      *
-      * This will emit a disconnected signal to whoever it's connected to.  In the case that even
-      * a single command has been sent to the server, the server will catch the signal and
-      * handle it accordingly.
-      *
-      */
-     void quitFromServer();
-     /**
-      * @brief Reads that the server this is connected to has been disconnected.
-      *
-      * If the server has been disconnected, this tcp socket has to be closed and the user
-      * should be notified that the server is no longer operation.  A new connection to a new
-      * server must be made (or the same server if the server is returned to life).
-      *
-      */
-     void serverDisconnected();
+    /**
+     * @brief quitFromServer quits from server
+     *
+     * This disconnects the client from the server and closes the socket.
+     */
+    void quitFromServer();
 
- private:
-     /**
-      * @brief Writes a command to the socket.
-      *
-      * This will first write data to a socket indicating how long this data will be. Then it writes
-      * the data itself along the communication and waits for the bytes to be written before returning.
-      *
-      * @param QString The command string that we wish to pass to the server.
-      */
-     void writeCommand(QString);
+    /**
+     * @brief login logs the user in
+     *
+     * This connects the user to the chat room and sends the username to the server.
+     * It also takes the serverName and port number from the GUI to connect to the server.
+     *
+     * @param name the username from the GUI
+     * @param serverName the server name the client is connecting to
+     * @param port the port number the client needs to connect to the server
+     */
+    void login(QString serverName, int port, QString name);
 
-     QLabel *hostLabel; /**< The label "Server Name:" */
-     QLabel *portLabel; /**< The label "Server Port:" */
-     QLabel *commandLabel; /**< The label "Command:" */
-     QLineEdit *hostLineEdit; /**< The editable box for the server name */
-     QLineEdit *portLineEdit; /**< The editable box for the port number */
-     QLineEdit *commandLineEdit; /**< The editable box for the command string */
-     QLabel *statusLabel; /**< The label displaying the result returned from the server */
-     QPushButton *sendCmdButton; /**< The button to send a command to the server */
-     QPushButton *quitButton; /**< The button to make the client quit and disconnect */
-     QDialogButtonBox *buttonBox; /**< Contains the two buttons */
+    /**
+     * @brief status updates the user's status
+     *
+     * Receives the user's status from the GUI and send its to the server.
+     *
+     * @param status the status entered by the user in the GUI
+     */
+    void status(QString status);
 
-     QTcpSocket *tcpSocket; /**< The tcp socket that this client communicates with */
-     quint16 blockSize; /**< The block size that will be read from the socket.  Resets on every new read */
+private slots:
+    /**
+     * @brief catchError Catches errors thrown
+     *
+     * Catches any errors thrown from the socket connection. Then emits sendError
+     * to send the error to print on the GUI
+     * @param socketError the error thrown from the socket connection
+     */
+    void catchError(QAbstractSocket::SocketError socketError);
 
-     QNetworkSession *networkSession; /**< The session with the server */
- };
+    /**
+     * @brief serverDisconnected slot to handle disconnects
+     *
+     * The slot handles disconnections. It shutdowns the socket if the server closes
+     * before the client.
+     */
+    void serverDisconnected();
 
- #endif
+    /**
+     * @brief readResults receives info from the socket
+     *
+     * The slot that actually receives the information from the socket. It then
+     * decodes the information and calls appropriate slots or functions that
+     * deal with the data sent.
+     */
+    void readResults();
+
+signals:
+    /**
+     * @brief sendError sends errors to the GUI
+     *
+     * Emitted in catchError. It will send the error caught by catchError to be
+     * printed in the GUI
+     *
+     * @param the error message to be printed
+     */
+    void sendError(QString error);
+
+    /**
+     * @brief newMessage a new message from another
+     *
+     * This is used to populate the GUI with a message generated from another user.
+     *
+     * @param user the username sending the message
+     * @param msg the actual message
+     */
+    void newMessage(QString user,QString msg);
+
+    /**
+     * @brief listUpdate sends the list of users to the GUI
+     *
+     * Takes the QMap of users and their statuses received from the server and
+     * sends it to the GUI
+     *
+     * @param map The QMap with all users and their statuses
+     */
+    void listUpdate(QMap<QString,QString>& map);
+
+    /**
+     * @brief connected tells GUI connected
+     *
+     * Emitted when a successful connection is established so the GUI can pop up and
+     * accept user input.
+     */
+    void connected();
+
+    /**
+     * @brief sendServerDisconnect sends a message to the GUI saying the server disconnected.
+     *
+     * This is called in serverDisconnected() and lets the GUI know that the server
+     * disconnected before the client closed so that it can prompt the user
+     */
+    void sendServerDisconnect();
+
+
+private:
+
+    /**
+     * @brief sendCommand sends a message to the server
+     *
+     * The function to actually send the messsage in the form that is readable by the server.
+     * It is called in
+     *
+     * @param msg the message that needs to be sent
+     */
+    void sendCommand(QString msg);
+
+
+    QTcpSocket *tcpSocket;/**< The tcp socket that this client communicates with */
+
+};
+
+#endif // CLIENT_H
